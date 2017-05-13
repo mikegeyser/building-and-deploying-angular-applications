6ef985519923f48d02113a1e637cff3e306b78c5
Mike Geyser <michaelg@bbd.co.za>
2017-05-13 13:55:57 +0200

    Got the basic demo working

---
+++ demo/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAZ9W3MpydRaIZt8RUBLA9Ag7uajjbdrwg",
+    authDomain: "coded-in-braam.firebaseapp.com",
+    databaseURL: "https://coded-in-braam.firebaseio.com",
+    projectId: "coded-in-braam",
+    storageBucket: "coded-in-braam.appspot.com",
+    messagingSenderId: "871071533911"
+  }
+};
